b897fe9acf2b95c26903b5a027c9f6187aa0e693
Juan David <159097264+jdym3119@users.noreply.github.com>
2024-02-28 21:29:40 -0500

    Update sum.py

--- sum.py
+++ sum.py
@@ -2,8 +2,17 @@
 list variable `sums` and then iterates through the elements of the input list `a`.'''
 def sums_nums(a):
     sums=[]
+    # The line `for i in range(len(a)-1):` is setting up a loop that iterates through the indices of
+    # the input list `a`, excluding the last index. The loop will run from 0 to `len(a)-2`, as
+    # `range(len(a)-1)` generates a sequence of numbers starting from 0 up to `len(a)-2`.
     for i in range(len(a)-1):
+        # The line `if i==0: sums.append(a[i]+a[i+1])` is a conditional statement within the loop.
         if i==0:sums.append(a[i]+a[i+1])
+        # The line `elif a[i]+a[i+1]>=a[i]+a[i-1]: sums.append(a[i]+a[i+1])` is a conditional
+        # statement within the loop.
         elif a[i]+a[i+1]>=a[i]+a[i-1]:sums.append(a[i]+a[i+1])
+        # The line `else: sums.append(a[i]+a[i-1])` is part of an `else` block in the loop.
         else:sums.append(a[i]+a[i-1])
+    # The line `return max(sums)` in the code snippet is returning the maximum value from the list
+    # `sums`.
     return max(sums)
