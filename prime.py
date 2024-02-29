485e9930792acd1220d45ef43578a6f9a6b47cda
Juan David <159097264+jdym3119@users.noreply.github.com>
2024-02-28 21:33:29 -0500

    Update prime.py

--- prime.py
+++ prime.py
@@ -9,10 +9,26 @@ def prime_number(a):
     :return: The function `prime_number` returns a list of prime numbers sorted in ascending order.
     """
     def is_prime(num):
+        """
+        This function checks if a given number is a prime number.       
+        :param num: A positive integer for which you want to check if it is a prime number
+        """
         for j in range(2, num):
+        # The line `for j in range(2, num):` in the `is_prime` function is creating a loop that
+        # iterates over a range of numbers from 2 to `num - 1`.
+        
             if num % j == 0:
                 return False
+            # The code snippet `if num % j == 0: return False` is checking if the given number `num` is divisible
+            # by the current number `j` without leaving a remainder.
         return True
+        # In the `is_prime` function, the line `return True` is used to indicate that the number being
+        # checked is a prime number. If the function reaches this point without finding any divisors
+        # for the given number `num`, it means that the number is not divisible by any number other
+        # than 1 and itself, hence it is considered a prime number. The `return True` statement
+        # signifies that the number is indeed a prime number.
     a.sort()
     primes = [i for i in a if is_prime(i)]
     return primes
+# The code snippet `a.sort()` is sorting the list of numbers in ascending order. After sorting the
+# list, the code then creates a new list called `primes` using a list comprehension.
