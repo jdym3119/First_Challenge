92bfe1109a7da63de2c0cd8383782bbdb09bdf9f
Juan David <159097264+jdym3119@users.noreply.github.com>
2024-02-28 21:39:33 -0500

    Update math_operations.py

--- math_operations.py
+++ math_operations.py
@@ -13,9 +13,22 @@ def basic_math_operations(x,y,z):
     :return: The function `basic_math_operations` takes three parameters `x`, `y`, and `z`. It performs
     a basic mathematical operation based on the value of `z` and returns the result.
     """
+    # The line `if z == '+': return x + y` in the `basic_math_operations` function is checking if the
+    # value of the parameter `z` is equal to the symbol '+'. If this condition is true, it means that
+    # the function should perform addition between the numbers `x` and `y`.
     if z=='+':return x+y
+    # The line `elif z=='-': return x-y` in the `basic_math_operations` function is checking if the
+    # value of the parameter `z` is equal to the symbol '-'. If this condition is true, it means that
+    # the function should perform subtraction between the numbers `x` and `y`. In other words, when
+    # `z` is '-', the function will subtract `y` from `x` and return the result.
     elif z=='-':return x-y
+    # The line `elif z=='*': return x*y` in the `basic_math_operations` function is checking if the
+    # value of the parameter `z` is equal to the symbol '*'. If this condition is true, it means that
+    # the function should perform multiplication between the numbers `x` and `y`. In other words, when
+    # `z` is '*', the function will multiply `x` by `y` and return the result.
     elif z=='*':return x*y
+    # The code snippet `elif z=='/': if y!=0: return x/y else: return "It\'s impossible."` is handling the
+    # division operation in the `basic_math_operations` function.
     elif z=='/':
         if y!=0: return x/y
         else: return "It\'s impossible."
